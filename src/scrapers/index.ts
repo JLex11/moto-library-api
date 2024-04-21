9cd113cf65e83222df0ee364e1b10aeedffda901
Jlex11 <chikyelfurioso2011@gmail.com>
2024-04-20 20:57:52 -0500

    refactor

--- src/scrapers/index.ts
+++ src/scrapers/index.ts
@@ -1,37 +1,40 @@
 import * as cheerio from 'cheerio'
 import { Motorcycle } from '../../types'
 import { scrapingMethods } from './data'
+import { ScrapeMotorcyclesFunction, ScrapedMotorcycle } from './types'
 
 const cheerioFromUrl = async (url: string) => {
   const html = await fetch(url).then(res => res.text())
   return cheerio.load(html)
 }
 
-export const getAllMotorcycles = async () => {
+const scrapeMotorcycles: ScrapeMotorcyclesFunction = async ($, getMotorcycles, getMotorcycleInfo) => {
+  const motorcyclesData = getMotorcycles($)
+
+  const motorcyclesInfoPromises = motorcyclesData.map<Promise<ScrapedMotorcycle>>(async motorcycleData => {
+    const { motorcycleUrl, motorcycleGroup } = motorcycleData
+    const $motorcyclePage = await cheerioFromUrl(motorcycleUrl)
+
+    const motorcycleInfo = getMotorcycleInfo($motorcyclePage)
+    motorcycleInfo.model ||= motorcycleUrl.split('/').findLast(part => part.match(/[a-z]+/)) || ''
+
+    return {
+      type: motorcycleGroup,
+      ...motorcycleInfo
+    }
+  })
+
+  const motorcyclesInfo = await Promise.all(motorcyclesInfoPromises)
+  return motorcyclesInfo
+}
+
+export const scrapeAllMotorcycles = async () => {
   const allMotorcyclesPromises = scrapingMethods.map(
     async ({ urls, brand, getMotorcycles, getMotorcycleInfo }) => {
       const motorcyclesPromises = urls.map<Promise<Motorcycle[]>>(async url => {
         const $ = await cheerioFromUrl(url)
-
-        const motorcyclesData = getMotorcycles($)
-        const motorcyclesInfoPromises = motorcyclesData.map<Promise<Motorcycle>>(async motorcycleData => {
-          const { motorcycleUrl, motorcycleGroup } = motorcycleData
-          const $motorcyclePage = await cheerioFromUrl(motorcycleUrl)
-
-          const motorcycleInfo = getMotorcycleInfo($motorcyclePage)
-          if (!motorcycleInfo.model) {
-            motorcycleInfo.model = motorcycleUrl.split('/').findLast(part => part.match(/[a-z]+/)) || ''
-          }
-
-          return {
-            brand,
-            type: motorcycleGroup,
-            ...motorcycleInfo
-          }
-        })
-
-        const motorcyclesInfo = await Promise.all(motorcyclesInfoPromises)
-        return motorcyclesInfo
+        const motorcycles = await scrapeMotorcycles($, getMotorcycles, getMotorcycleInfo)
+        return motorcycles.map(motorcycle => ({ ...motorcycle, brand }))
       })
 
       const motorcycles = await Promise.all(motorcyclesPromises)
