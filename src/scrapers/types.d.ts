1555969312d8926b7050c194afbe2a1d1dd3c2c1
Jlex11 <chikyelfurioso2011@gmail.com>
2024-04-20 20:58:38 -0500

    adding some types

--- src/scrapers/types.d.ts
+++ src/scrapers/types.d.ts
@@ -1,5 +1,23 @@
+import { CheerioAPI } from 'cheerio'
+import { Motorcycle, MotorcycleInfo } from '../../types'
+
+export interface ScrapingMethod {
+  brand: string
+  urls: string[]
+  getMotorcycles: ($page: CheerioAPI) => MotorcyclesData[]
+  getMotorcycleInfo: ($page: CheerioAPI) => MotorcycleInfo
+}
+
 export interface MotorcyclesData {
   motorcycleName: string
   motorcycleUrl: string
   motorcycleGroup?: string
 }
+
+type ScrapedMotorcycle = Omit<Motorcycle, 'brand'>
+
+type ScrapeMotorcyclesFunction = (
+  $: CheerioAPI,
+  getMotorcycles: ScrapingMethod['getMotorcycles'],
+  getMotorcycleInfo: ScrapingMethod['getMotorcycleInfo']
+) => Promise<ScrapedMotorcycle[]>
