eb95e7caa22daf547ba4fd0de02599c114f5bd36
Jlex11 <chikyelfurioso2011@gmail.com>
2024-04-20 20:58:28 -0500

    using hono cache

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,13 @@
 import { Hono } from 'hono'
-import { getAllMotorcycles } from './scrapers'
+import { cache } from 'hono/cache'
+import { scrapeAllMotorcycles } from './scrapers'
 
 const app = new Hono()
 
+app.get('/*', cache({ cacheName: 'motorcycles', cacheControl: 'max-age=3600' }))
+
 app.get('/', async c => {
-  const allMotorcycles = await getAllMotorcycles()
+  const allMotorcycles = await scrapeAllMotorcycles()
   return c.json(allMotorcycles)
 })
 
