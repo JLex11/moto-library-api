444f8da52045ea8bec19bdf839d5791301d36d15
Jlex11 <chikyelfurioso2011@gmail.com>
2024-05-11 19:42:21 -0500

    add import alias

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,9 @@
+import { getAllMotorcycles } from '@/scrapers'
 import { Hono } from 'hono'
-import { cache } from 'hono/cache'
-import { getAllMotorcycles } from './scrapers'
 
 const app = new Hono()
 
-app.get('/*', cache({ cacheName: 'motorcycles', cacheControl: 'max-age=3600' }))
+//app.get('/*', cache({ cacheName: 'motorcycles', cacheControl: 'max-age=3600' }))
 
 app.get('/', async c => {
   const allMotorcycles = await getAllMotorcycles()
