d2d61a391b6682784cfb746ec7c4727ce3ee1f74
Jlex11 <chikyelfurioso2011@gmail.com>
2024-05-11 19:39:58 -0500

    fix image paths

--- src/scrapers/methods/suzuki/lib/getImages.ts
+++ src/scrapers/methods/suzuki/lib/getImages.ts
@@ -1,12 +1,14 @@
 import { CheerioAPI } from 'cheerio'
 import { MotorcycleImages } from '../../../../../types'
+import { DOMAIN } from '../constants'
 
 export const getImages = ($page: CheerioAPI): MotorcycleImages => {
   const colorImages = [...$page('.carousel-indicators.carousel-indicators-numbers li')].map(colorImgCheck => {
     const $colorImgCheck = $page(colorImgCheck)
     const dataSlideTo = $colorImgCheck.attr('data-slide-to')!
 
-    const imageSrc = $page(`.carousel-inner .item:nth-child(${Number(dataSlideTo) + 1}) img`).attr('src')!
+    const imagePath = $page(`.carousel-inner .item:nth-child(${Number(dataSlideTo) + 1}) img`).attr('src')!
+    const imageSrc = `${DOMAIN}${imagePath}`
     const color = $colorImgCheck.css('background-color') || ''
 
     return { color, image: imageSrc }
@@ -14,7 +16,8 @@ export const getImages = ($page: CheerioAPI): MotorcycleImages => {
 
   const gallery = [...$page('.galeria-moto .img-galeria')].map(galleryItem => {
     const $galleryItem = $page(galleryItem)
-    const imageSrc = $galleryItem.css('background-image')!.replace(/url\("(.*)"\)/, '$1')
+    const imagePath = $galleryItem.css('background-image')!.replace(/url\(("|')(.*)("|')\)/, '$2')
+    const imageSrc = `${DOMAIN}${imagePath}`
     const imageTitle = $galleryItem.find('a').text()
 
     return { image: imageSrc, title: imageTitle }
