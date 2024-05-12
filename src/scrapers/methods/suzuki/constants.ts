d06f8ec8f83733684842505089a3d4e83d0f6e81
Jlex11 <chikyelfurioso2011@gmail.com>
2024-05-11 19:40:25 -0500

    fix urls

--- src/scrapers/methods/suzuki/constants.ts
+++ src/scrapers/methods/suzuki/constants.ts
@@ -1,6 +1,6 @@
 export const BRAND_NAME = 'suzuki'
-export const BRAND_URL = 'https://www.suzuki.com.co/'
-export const URLS = ['https://www.suzuki.com.co/motocicletas/lista']
+export const DOMAIN = 'https://suzuki.com.co'
+export const URLS = ['https://suzuki.com.co/motocicletas/lista']
 
 export const scrapingData = {
   brand: BRAND_NAME,
