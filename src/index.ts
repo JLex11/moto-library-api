9d19982c4fe4f930cd5a1ca077ebec56cbdb1aed
Jlex11 <chikyelfurioso2011@gmail.com>
2024-05-17 23:41:06 -0500

    using database

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,22 @@
-import { getAllMotorcycles } from '@/scrapers'
 import { Hono } from 'hono'
+import { cache } from 'hono/cache'
 
-const app = new Hono()
+type Bindings = {
+  DB: D1Database
+}
 
-//app.get('/*', cache({ cacheName: 'motorcycles', cacheControl: 'max-age=3600' }))
+const app = new Hono<{ Bindings: Bindings }>()
+
+app.get('/*', cache({ cacheName: 'motorcycles', cacheControl: 'max-age=3600' }))
 
 app.get('/', async c => {
-  const allMotorcycles = await getAllMotorcycles()
-  return c.json(allMotorcycles)
+  // const allMotorcycles = await getAllMotorcycles()
+  try {
+    const { results } = await c.env.DB.prepare('SELECT * FROM Motorcycle').all()
+    return c.json(results)
+  } catch (error) {
+    return c.json({ error }, 500)
+  }
 })
 
 export default app
